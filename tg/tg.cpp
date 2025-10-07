6251746cfea5ec152557ddb3f429302c3c5a242d
Anastasia Gozheva <d1804a@gmail.com>
2025-10-07 06:20:51 +0400

    add flags input (still bint)

--- tg/tg.cpp
+++ tg/tg.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <variant>
 
 #include "graphEasier.h"
 
@@ -7,14 +8,57 @@
 using namespace std;
 
 
-int main()
-{
-    std::cout << "Hello!\n";
-    graph<int, int> tr(false, false);
-    int n = tr.size();
-    tr.input();
-    tr.output();
-    std::cout << "Hello World!\n";
+void myChoiceAssert(bool smth) {
+	if (!smth) throw exception("Введен некорректный вариант ответа");
+}
 
-    //cin >> n;
+struct typesOfGraphs {
+	graph<int, int>			gii = graph<int, int>		(false, false, false);
+	graph<int, string>		gis = graph<int, string>	(false, false, false);
+	graph<string, int>		gsi = graph<string, int>	(false, false, false);
+	graph<string, string>	gss = graph<string, string>	(false, false, false);
+
+
+};
+
+int main() {
+	setlocale(LC_ALL, "ru-RU");
+
+	while (true) {
+		try {
+			cout << "Составим идеальный граф для вашей задачи ^^\n";
+			cout << "Должен ли он быть взвешенным? (y/n): ";
+			char isWeighted;
+			cin >> isWeighted; myChoiceAssert(isWeighted == 'y' || isWeighted == 'n');
+			cout << "Подойдет ли ориентированный?  (y/n): ";
+			char isOrdered;
+			cin >> isOrdered; myChoiceAssert(isOrdered == 'y' || isOrdered == 'n');
+			cout << "Вам нужны метки на ребрах?    (y/n): ";
+			char isMarked;
+			cin >> isMarked; myChoiceAssert(isMarked == 'y' || isMarked == 'n');
+			string markT = "int", vertexT;
+			if (isMarked == 'y') {
+				cout << "Какого типа будут метки?  (str/int): ";
+				cin >> markT;  myChoiceAssert(markT == "str" || markT == "int");
+			}
+			cout << "А какие нужны вершины?    (str/int): ";
+			cin >> vertexT; myChoiceAssert(vertexT == "str" || vertexT == "int");
+
+			//graph<, >g(isOrdered - 'n', isWeighted - 'n', isMarked - 'n');
+			variant<graph<int, int>, graph<int, string>, graph<string, int>, graph<string, string>> g;
+
+
+
+
+
+
+		} catch (exception e) {
+			cout << "Что-то странное...\n";
+			cout << "Сообщение ошибки: " << e.what() << '\n';
+			cout << "попробуйте заново, что ли...\n";
+		} catch (...) {
+			cout << "Что-то странное... попробуйте заново, что ли...\n";
+		}
+		cout << "+--------------------------------------------------------------------+\n";
+	}
 }
